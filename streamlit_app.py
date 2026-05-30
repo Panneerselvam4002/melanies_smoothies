f6449249f553600ab15a977e3c573ca4ad1cbd63
Panneerselvam4002 <ppanneerselvam204@gmail.com>
2026-05-30 15:35:41 +0530

    Update streamlit_app.py
    connected changes needed bc of move from sis to snis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
