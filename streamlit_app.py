01cf81220633dd518006069231b52139e7c89862
Panneerselvam4002 <ppanneerselvam204@gmail.com>
2026-05-31 17:51:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,8 @@ if ingredients_list:
 
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
-        conn.query(my_insert_stmt)
+        with conn.session() as session:
+            session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 
